c4b73f3b9da12790402d2eed9f2bd8e38722bfdf
SizzlingCalamari <jordan.cristi@gmail.com>
2023-02-09 02:48:52 -0500

    Added hook for VoiceData Process

--- ServerPlugin/ServerPlugin.cpp
+++ ServerPlugin/ServerPlugin.cpp
@@ -3,6 +3,8 @@
 #include "sourcesdk/public/eiface.h"
 #include "sourcesdk/public/iserver.h"
 #include "sourcesdk/public/iclient.h"
+#include "sourcesdk/public/inetchannel.h"
+#include "sourcesdk/public/inetmessage.h"
 #include <string.h>
 
 class ServerPlugin : public IServerPluginCallbacks
@@ -11,7 +13,7 @@ public:
     ServerPlugin();
     virtual ~ServerPlugin() {}
     virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory);
-    virtual void Unload(void) {}
+    virtual void Unload(void);
     virtual void Pause(void) {}
     virtual void UnPause(void) {}
     virtual const char* GetPluginDescription(void) { return "ServerPlugin"; }
@@ -71,9 +73,123 @@ bool ServerPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn ga
     return mVEngineServer && mServer;
 }
 
+template<typename T, typename U>
+inline T ByteOffsetFromPointer(U pBase, int byte_offset)
+{
+    return reinterpret_cast<T>((reinterpret_cast<unsigned char*>(pBase) + byte_offset));
+}
+
+template<typename T>
+struct CUtlMemory
+{
+    T* m_pMemory;
+    int m_nAllocationCount;
+    int m_nGrowSize;
+};
+
+template<typename T>
+struct CUtlVector
+{
+    CUtlMemory<T> m_Memory;
+    int m_Size;
+    T* m_pElements;
+};
+
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+class IVoiceDataHook;
+using INetMessage_ProcessPtr = bool (INetMessage::*)();
+using IVoiceDataHook_ProcessPtr = bool (IVoiceDataHook::*)();
+
+class IVoiceDataHook : public INetMessage
+{
+public:
+    bool ProcessHook()
+    {
+        return (this->*sVoiceDataProcessFn)();
+    }
+
+    static bool IsVoiceDataHooked()
+    {
+        return sVoiceDataProcessFn;
+    }
+
+    static void RegisterProcessHook(INetMessage* NetMsg)
+    {
+        if (!sVoiceDataProcessFn)
+        {
+            void** voiceDataVtable = *(void***)NetMsg;
+
+            
+            INetMessage_ProcessPtr ProcessPtr = (INetMessage_ProcessPtr&)voiceDataVtable[ProcessOffset];
+            IVoiceDataHook_ProcessPtr NewProcessPtr = &IVoiceDataHook::ProcessHook;
+
+            DWORD oldProtect;
+            DWORD tempProtect;
+            int ret = VirtualProtect(&voiceDataVtable[ProcessOffset], 4, PAGE_EXECUTE_READWRITE, &oldProtect);
+            memcpy(&voiceDataVtable[ProcessOffset], &NewProcessPtr, 4);
+            VirtualProtect(&voiceDataVtable[ProcessOffset], 4, oldProtect, &tempProtect);
+
+            sVoiceDataVTable = voiceDataVtable;
+            sVoiceDataProcessFn = ProcessPtr;
+        }
+    }
+
+    static void UnregisterProcessHook()
+    {
+        if (sVoiceDataProcessFn)
+        {
+            void** voiceDataVtable = sVoiceDataVTable;
+            void* processSlot = &voiceDataVtable[ProcessOffset];
+
+            DWORD oldProtect;
+            DWORD tempProtect;
+            VirtualProtect(processSlot, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
+            memcpy(processSlot, &sVoiceDataProcessFn, 4);
+            VirtualProtect(processSlot, 4, oldProtect, &tempProtect);
+
+            sVoiceDataProcessFn = nullptr;
+            sVoiceDataVTable = nullptr;
+        }
+    }
+
+private:
+    static constexpr int ProcessOffset = 3;
+    static void** sVoiceDataVTable;
+    static INetMessage_ProcessPtr sVoiceDataProcessFn;
+};
+
+void** IVoiceDataHook::sVoiceDataVTable = nullptr;
+INetMessage_ProcessPtr IVoiceDataHook::sVoiceDataProcessFn = nullptr;
+
+void ServerPlugin::Unload(void)
+{
+    IVoiceDataHook::UnregisterProcessHook();
+}
+
 void ServerPlugin::ClientActive(edict_t* pEntity)
 {
-    const int entIndex = mVEngineServer->IndexOfEdict(pEntity);
-    IClient* client = mServer->GetClient(entIndex - 1);
-    INetChannel* netChannel = client->GetNetChannel();
+    if (!IVoiceDataHook::IsVoiceDataHooked())
+    {
+        const int entIndex = mVEngineServer->IndexOfEdict(pEntity);
+        IClient* client = mServer->GetClient(entIndex - 1);
+        INetChannel* netChannel = client->GetNetChannel();
+        if (netChannel)
+        {
+            //netChannel->RegisterMessage(nullptr);
+
+            CUtlVector<INetMessage*>* pNetMessages = ByteOffsetFromPointer<CUtlVector<INetMessage*>*>(netChannel, 9000);
+            for (int i = 0; i < pNetMessages->m_Size; ++i)
+            {
+                INetMessage* msg = pNetMessages->m_pElements[i];
+                const char* name = msg->GetName();
+                if (!strcmp(name, "clc_VoiceData"))
+                {
+                    IVoiceDataHook::RegisterProcessHook(msg);
+                }
+            }
+        }
+    }
 }
