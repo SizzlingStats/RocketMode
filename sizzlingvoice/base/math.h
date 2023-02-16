cad18025017b9ed63b11410277488eda5e9f00b9
SizzlingCalamari <jordan.cristi@gmail.com>
2023-02-16 02:15:14 -0500

    Added min and max functions.

--- sizzlingvoice/base/math.h
+++ sizzlingvoice/base/math.h
@@ -9,6 +9,16 @@ namespace Math
     float Cos(float x);
     float Sin(float x);
 
+    FORCEINLINE float Min(float x, float y)
+    {
+        return x < y ? x : y;
+    }
+
+    FORCEINLINE float Max(float x, float y)
+    {
+        return x > y ? x : y;
+    }
+
     FORCEINLINE float Clamp(float x, float min, float max)
     {
         return (x < min) ? min : ((x > max) ? max : x);
