2f03c442b464d4133b75beff3de974b1536938f1
SizzlingCalamari <jordan.cristi@gmail.com>
2023-02-27 23:54:40 -0500

    Reduced autotalent dsp size by 500 bytes.

--- sizzlingvoice/dsp/autotalent.h
+++ sizzlingvoice/dsp/autotalent.h
@@ -3,38 +3,42 @@
 
 #include "autotalent/ladspa.h"
 
-#define AT_TUNE 0
-#define AT_FIXED 1
-#define AT_PULL 2
-#define AT_A 3
-#define AT_Bb 4
-#define AT_B 5
-#define AT_C 6
-#define AT_Db 7
-#define AT_D 8
-#define AT_Eb 9
-#define AT_E 10
-#define AT_F 11
-#define AT_Gb 12
-#define AT_G 13
-#define AT_Ab 14
-#define AT_AMOUNT 15
-#define AT_SMOOTH 16
-#define AT_SHIFT 17
-#define AT_SCWARP 18
-#define AT_LFOAMP 19
-#define AT_LFORATE 20
-#define AT_LFOSHAPE 21
-#define AT_LFOSYMM 22
-#define AT_LFOQUANT 23
-#define AT_FCORR 24
-#define AT_FWARP 25
-#define AT_MIX 26
-#define AT_PITCH 27
-#define AT_CONF 28
-#define AT_INPUT1  29
-#define AT_OUTPUT1 30
-#define AT_LATENCY 31
+enum AT_Ports
+{
+    AT_CONCERT_A = 0,
+    AT_FIXED_PITCH_SEMITONES = 1,
+    AT_FIXED_PITCH_DEGREE = 2,
+    AT_A = 3,
+    AT_Bb = 4,
+    AT_B = 5,
+    AT_C = 6,
+    AT_Db = 7,
+    AT_D = 8,
+    AT_Eb = 9,
+    AT_E = 10,
+    AT_F = 11,
+    AT_Gb = 12,
+    AT_G = 13,
+    AT_Ab = 14,
+    AT_CORRECTION_STRENGTH = 15,
+    AT_CORRECTION_SMOOTHNESS = 16,
+    AT_PITCH_SHIFT_NOTES = 17,
+    AT_OUTPUT_SCALE_ROTATE = 18,
+    AT_LFO_DEGREE = 19,
+    AT_LFO_RATE = 20,
+    AT_LFO_SHAPE = 21,
+    AT_LFO_SYMMETRY = 22,
+    AT_LFO_QUANTIZATION = 23,
+    AT_FORMANT_CORRECTION = 24,
+    AT_FORMANT_WARP = 25,
+    AT_MIX = 26,
+    AT_PITCH_OUTPUT = 27,
+    AT_CONFIDENCE_OUTPUT = 28,
+    AT_INPUT1 = 29,
+    AT_OUTPUT1 = 30,
+    AT_LATENCY_OUTPUT = 31,
+    AT_NUM_PORTS
+};
 
 //                                   A, Bb,  B,  C, Db,  D, Eb,  E,  F, Gb,  G, Ab
 static float sCMajor[12]        = {  1, -1,  1,  1, -1,  1, -1,  1,  1, -1,  1, -1 };
@@ -53,6 +57,42 @@ public:
     static void GlobalInit() { _init(); }
     static void GlobalShutdown() { _fini(); }
 
+    AutoTalent()
+    {
+        mPortSettings[AT_CONCERT_A] = 440.0f;
+        mPortSettings[AT_FIXED_PITCH_SEMITONES] = 0.0f;
+        mPortSettings[AT_FIXED_PITCH_DEGREE] = 0.0f;
+        mPortSettings[AT_A] = 1.0f;
+        mPortSettings[AT_Bb] = 1.0f;
+        mPortSettings[AT_B] = 1.0f;
+        mPortSettings[AT_C] = 1.0f;
+        mPortSettings[AT_Db] = 1.0f;
+        mPortSettings[AT_D] = 1.0f;
+        mPortSettings[AT_Eb] = 1.0f;
+        mPortSettings[AT_E] = 1.0f;
+        mPortSettings[AT_F] = 1.0f;
+        mPortSettings[AT_Gb] = 1.0f;
+        mPortSettings[AT_G] = 1.0f;
+        mPortSettings[AT_Ab] = 1.0f;
+        mPortSettings[AT_CORRECTION_STRENGTH] = 1.0f;
+        mPortSettings[AT_CORRECTION_SMOOTHNESS] = 0.0f;
+        mPortSettings[AT_PITCH_SHIFT_NOTES] = 0.0f;
+        mPortSettings[AT_OUTPUT_SCALE_ROTATE] = 0.0f;
+        mPortSettings[AT_LFO_DEGREE] = 0.0f;
+        mPortSettings[AT_LFO_RATE] = 5.0f;
+        mPortSettings[AT_LFO_SHAPE] = 1.0f;
+        mPortSettings[AT_LFO_SYMMETRY] = 1.0f;
+        mPortSettings[AT_LFO_QUANTIZATION] = 0.0f;
+        mPortSettings[AT_FORMANT_CORRECTION] = 0.0f;
+        mPortSettings[AT_FORMANT_WARP] = 0.0f;
+        mPortSettings[AT_MIX] = 1.0f;
+        mPortSettings[AT_PITCH_OUTPUT] = 0.0f;
+        mPortSettings[AT_CONFIDENCE_OUTPUT] = 0.0f;
+        mPortSettings[AT_INPUT1] = 0.0f;
+        mPortSettings[AT_OUTPUT1] = 0.0f;
+        mPortSettings[AT_LATENCY_OUTPUT] = 0.0f;
+    }
+
     void SetKey(float(&key)[12])
     {
         for (int i = AT_A; i <= AT_Ab; ++i)
@@ -66,30 +106,12 @@ public:
         mDescriptor = ladspa_descriptor(0);
         mHandle = mDescriptor->instantiate(mDescriptor, 22050);
 
-        mDescriptor->connect_port(mHandle, AT_TUNE, &AHz);
-        mDescriptor->connect_port(mHandle, AT_FIXED, &fixedPitchSemitones);
-        mDescriptor->connect_port(mHandle, AT_PULL, &fixedPitchDegree);
+        for (int i = 0; i < AT_NUM_PORTS; ++i)
+        {
+            mDescriptor->connect_port(mHandle, i, &mPortSettings[i]);
+        }
         
         SetKey(sCMajor);
-
-        mDescriptor->connect_port(mHandle, AT_AMOUNT, &correctionStrength);
-        mDescriptor->connect_port(mHandle, AT_SMOOTH, &correctionSmoothness);
-        mDescriptor->connect_port(mHandle, AT_SHIFT, &pitchShift);
-        mDescriptor->connect_port(mHandle, AT_SCWARP, &outputScaleRotation);
-        mDescriptor->connect_port(mHandle, AT_LFOAMP, &lfoDepth);
-        mDescriptor->connect_port(mHandle, AT_LFORATE, &lfoRate);
-        mDescriptor->connect_port(mHandle, AT_LFOSHAPE, &lfoShape);
-        mDescriptor->connect_port(mHandle, AT_LFOSYMM, &lfoSymmetry);
-        mDescriptor->connect_port(mHandle, AT_LFOQUANT, &lfoQuant);
-        mDescriptor->connect_port(mHandle, AT_FCORR, &formantCorrection);
-        mDescriptor->connect_port(mHandle, AT_FWARP, &formantWarp);
-        mDescriptor->connect_port(mHandle, AT_MIX, &mix);
-
-        mDescriptor->connect_port(mHandle, AT_PITCH, &pitch);
-        mDescriptor->connect_port(mHandle, AT_CONF, &confidence);
-        mDescriptor->connect_port(mHandle, AT_INPUT1, nullptr);
-        mDescriptor->connect_port(mHandle, AT_OUTPUT1, nullptr);
-        mDescriptor->connect_port(mHandle, AT_LATENCY, &latency);
     }
 
     void ProcessBuffer(float* samples, int numSamples)
@@ -107,27 +129,5 @@ private:
     const LADSPA_Descriptor* mDescriptor = nullptr;
     LADSPA_Handle mHandle = nullptr;
 
-    float fOne = 1.0f;
-    float fZero = 0.0f;
-    float fNegOne = -1.0f;
-
-    float AHz = 440.0f;
-    float fixedPitchSemitones = 0.0f;
-    float fixedPitchDegree = 0.0f;
-    float correctionStrength = 1.0f;
-    float correctionSmoothness = 0.0f;
-    float pitchShift = 0.0f;
-    float outputScaleRotation = 0.0f;
-    float lfoDepth = 0.0f;
-    float lfoRate = 5.0f;
-    float lfoShape = 1.0f;
-    float lfoSymmetry = 1.0f;
-    float lfoQuant = 0.0f;
-    float formantCorrection = 0.0f;
-    float formantWarp = 0.0f;
-    float mix = 1.0f;
-
-    float pitch = 0.0f;
-    float confidence = 0.0f;
-    float latency = 0.0f;
+    float mPortSettings[AT_NUM_PORTS];
 };
