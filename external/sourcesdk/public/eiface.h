fd6a16082bfba852fa93f5d51288e11e483e9e1d
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-04 05:04:29 -0500

    Fixed value of MAX_EDICT_BITS.

--- external/sourcesdk/public/eiface.h
+++ external/sourcesdk/public/eiface.h
@@ -5,7 +5,7 @@
 #define ABSOLUTE_PLAYER_LIMIT 255
 
 // How many bits to use to encode an edict.
-#define	MAX_EDICT_BITS				11			// # of bits needed to represent max edicts
+#define	MAX_EDICT_BITS				12			// # of bits needed to represent max edicts
 // Max # of edicts in a level
 #define	MAX_EDICTS					(1<<MAX_EDICT_BITS)
 
