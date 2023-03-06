49aeb7845c3ee93414da6f8802e4ed882018d516
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-06 04:32:26 -0500

    Fixed NUM_ENT_ENTRIES and friends defines to be in sync with live.

--- external/sourcesdk/public/const.h
+++ external/sourcesdk/public/const.h
@@ -10,9 +10,12 @@
 #define	MAX_EDICTS					(1<<MAX_EDICT_BITS)
 
 // Used for networking ehandles.
-#define NUM_ENT_ENTRY_BITS		(MAX_EDICT_BITS + 1)
-#define NUM_ENT_ENTRIES			(1 << NUM_ENT_ENTRY_BITS)
-#define ENT_ENTRY_MASK			(NUM_ENT_ENTRIES - 1)
-#define INVALID_EHANDLE_INDEX	0xFFFFFFFF
+#define NUM_ENT_ENTRY_BITS      (MAX_EDICT_BITS + 2)
+#define NUM_ENT_ENTRIES         (1 << NUM_ENT_ENTRY_BITS)
+#define INVALID_EHANDLE_INDEX   0xFFFFFFFF
+
+#define NUM_SERIAL_NUM_BITS     16 // (32 - NUM_ENT_ENTRY_BITS)
+#define NUM_SERIAL_NUM_SHIFT_BITS (32 - NUM_SERIAL_NUM_BITS)
+#define ENT_ENTRY_MASK          (( 1 << NUM_SERIAL_NUM_BITS) - 1)
 
 #define FL_ATCONTROLS			(1<<7) // Player can't move, but keeps key inputs for controlling another entity
