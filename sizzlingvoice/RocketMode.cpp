1a8aa953e18f76efcbd363c352733b38fa6a22bf
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-05 22:48:00 -0500

    Bit of style cleanup for RocketMode.

--- sizzlingvoice/RocketMode.cpp
+++ sizzlingvoice/RocketMode.cpp
@@ -47,55 +47,69 @@ static CBaseEntity* GetBaseEntity(edict_t* edict)
     return nullptr;
 }
 
+static const char* GetClassname(edict_t* edict)
+{
+    if (edict)
+    {
+        if (IServerNetworkable* networkable = edict->m_pNetworkable)
+        {
+            return networkable->GetClassName();
+        }
+    }
+    return nullptr;
+}
+
 void RocketMode::GameFrame(bool simulating)
 {
     for (edict_t* edict : mTrackingForBaseEnt)
     {
-        if (CBaseEntity* ent = GetBaseEntity(edict))
+        CBaseEntity* ent = GetBaseEntity(edict);
+        if (!ent)
         {
-            if (ent && edict->m_pNetworkable)
+            continue;
+        }
+
+        const char* classname = GetClassname(edict);
+        if (!classname || strcmp(classname, "tf_projectile_rocket"))
+        {
+            continue;
+        }
+
+        static const int offset = DatamapHelpers::GetDatamapVarOffsetFromEnt(ent, "m_hOwnerEntity");
+        assert(offset > 0);
+
+        CBaseHandle owningEntHandle = *(CBaseHandle*)((char*)ent + offset);
+        if (owningEntHandle.IsValid())
+        {
+            const int owningEntIndex = owningEntHandle.GetEntryIndex();
+            const int owningClientIndex = owningEntIndex - 1;
+            assert(owningClientIndex < MAX_PLAYERS);
+
+            IClient* client = mServer->GetClient(owningClientIndex);
+            if (client && !client->IsFakeClient())
             {
-                const char* classname = edict->m_pNetworkable->GetClassName();
-                if (!strcmp(classname, "tf_projectile_rocket"))
+                SVC_SetView setview;
+                setview.m_nEntityIndex = edict->m_EdictIndex;
+
+                if (client->SendNetMsg(setview, true))
                 {
-                    SVC_SetView setview;
-                    setview.m_nEntityIndex = edict->m_EdictIndex;
+                    mClientStates[owningClientIndex].rocket = edict;
 
-                    static const int offset = DatamapHelpers::GetDatamapVarOffsetFromEnt(ent, "m_hOwnerEntity");
-                    if (offset > 0)
+                    edict_t* owningEdict = mVEngineServer->PEntityOfEntIndex(owningEntIndex);
+                    assert(owningEdict);
+                    if (CBaseEntity* owningEnt = GetBaseEntity(owningEdict))
                     {
-                        CBaseHandle owningEntHandle = *(CBaseHandle*)((char*)ent + offset);
-                        if (owningEntHandle.IsValid())
-                        {
-                            const int owningEntIndex = owningEntHandle.GetEntryIndex();
-                            const int owningClientIndex = owningEntIndex - 1;
-                            assert(owningClientIndex < MAX_PLAYERS);
-                            IClient* client = mServer->GetClient(owningClientIndex);
-                            if (client && !client->IsFakeClient())
-                            {
-                                if (true)//client->SendNetMsg(setview, true))
-                                {
-                                    mClientStates[owningClientIndex].rocket = edict;
-
-                                    edict_t* owningEdict = mVEngineServer->PEntityOfEntIndex(owningEntIndex);
-                                    assert(owningEdict);
-                                    if (CBaseEntity* owningEnt = GetBaseEntity(owningEdict))
-                                    {
-                                        const int fFlagsOffset = 320;
-                                        *(int*)((char*)owningEnt + fFlagsOffset) |= FL_ATCONTROLS;
-                                        EdictChangeHelpers::StateChanged(owningEdict, fFlagsOffset, mVEngineServer);
-                                    }
-                                }
-
-                                // TODO: hook g_CommentarySystem.PrePlayerRunCommand as a pre usercmd process to clear weapon flags.
-                                // TODO: set flags to FL_ATCONTROLS so buttons are still passed through
-                                // TODO: hook g_pGameMovement->ProcessMovement for usercmds to get buttons
-
-                                //mServerTools->GetKeyValue()
-                            }
-                        }
+                        const int fFlagsOffset = 320;
+                        *(int*)((char*)owningEnt + fFlagsOffset) |= FL_ATCONTROLS;
+                        EdictChangeHelpers::StateChanged(owningEdict, fFlagsOffset, mVEngineServer);
                     }
                 }
+
+                // TODO: hook g_CommentarySystem.PrePlayerRunCommand as a pre usercmd process to clear weapon flags.
+                // TODO: set flags to FL_ATCONTROLS so buttons are still passed through
+                // TODO: hook g_pGameMovement->ProcessMovement for usercmds to get buttons
+
+                //mServerTools->GetKeyValue()
             }
         }
     }
