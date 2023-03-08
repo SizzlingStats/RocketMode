59516d9fbc56956f8088cd6f9e803a464f7b7579
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-08 00:22:36 -0500

    Fixed old sdk compat.

--- external/sourcesdk/public/const.h
+++ external/sourcesdk/public/const.h
@@ -10,12 +10,24 @@
 #define	MAX_EDICTS					(1<<MAX_EDICT_BITS)
 
 // Used for networking ehandles.
+#ifdef SDK_COMPAT
+#define NUM_ENT_ENTRY_BITS      (MAX_EDICT_BITS + 1)
+#else
 #define NUM_ENT_ENTRY_BITS      (MAX_EDICT_BITS + 2)
+#endif
 #define NUM_ENT_ENTRIES         (1 << NUM_ENT_ENTRY_BITS)
 #define INVALID_EHANDLE_INDEX   0xFFFFFFFF
 
+#ifdef SDK_COMPAT
+#define NUM_SERIAL_NUM_BITS     (32 - NUM_ENT_ENTRY_BITS)
+#else
 #define NUM_SERIAL_NUM_BITS     16 // (32 - NUM_ENT_ENTRY_BITS)
+#endif
 #define NUM_SERIAL_NUM_SHIFT_BITS (32 - NUM_SERIAL_NUM_BITS)
+#ifdef SDK_COMPAT
+#define ENT_ENTRY_MASK			(NUM_ENT_ENTRIES - 1)
+#else
 #define ENT_ENTRY_MASK          (( 1 << NUM_SERIAL_NUM_BITS) - 1)
+#endif
 
 #define FL_ATCONTROLS			(1<<7) // Player can't move, but keeps key inputs for controlling another entity
