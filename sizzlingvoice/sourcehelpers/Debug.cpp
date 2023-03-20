16caa7aef091ce41f4a68279e20a5809f097fce0
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-20 01:42:03 -0400

    Fixed tier0 shared object name on linux.

--- sizzlingvoice/sourcehelpers/Debug.cpp
+++ sizzlingvoice/sourcehelpers/Debug.cpp
@@ -5,9 +5,17 @@
 
 Debug::MsgFn* Debug::Msg;
 
+#ifdef _WIN32
+#define TIER0_MODULE "tier0.dll"
+#else
+#define TIER0_MODULE "libtier0_srv.so"
+#endif
+
 void Debug::Initialize()
 {
-    Platform::HModule hTier0 = Platform::GetModuleHandle("tier0");
+    Platform::HModule hTier0 = Platform::GetModuleHandle(TIER0_MODULE);
+    assert(hTier0);
 
     Debug::Msg = (MsgFn*)Platform::GetProcAddress(hTier0, "Msg");
+    assert(Debug::Msg);
 }
--- sizzlingvoice/sourcehelpers/ValveMemAlloc.cpp
+++ sizzlingvoice/sourcehelpers/ValveMemAlloc.cpp
@@ -6,11 +6,17 @@
 class IMemAlloc;
 IMemAlloc* g_pMemAlloc;
 
+#ifdef _WIN32
+#define TIER0_MODULE "tier0.dll"
+#else
+#define TIER0_MODULE "libtier0_srv.so"
+#endif
+
 bool ValveMemAlloc::Init()
 {
     assert(!g_pMemAlloc);
 
-    Platform::HModule hTier0 = Platform::GetModuleHandle("tier0.dll");
+    Platform::HModule hTier0 = Platform::GetModuleHandle(TIER0_MODULE);
     assert(hTier0);
 
     g_pMemAlloc = *reinterpret_cast<IMemAlloc**>(Platform::GetProcAddress(hTier0, "g_pMemAlloc"));
