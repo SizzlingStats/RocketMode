7ca484593bfb79691e468dc308e0b8b5c9cb7ed8
SizzlingCalamari <jordan.cristi@gmail.com>
2023-03-20 05:36:14 -0400

    Fixed IMemAlloc declaration on linux vs windows.

--- external/sourcesdk/public/tier0/memalloc.h
+++ external/sourcesdk/public/tier0/memalloc.h
@@ -4,6 +4,7 @@
 class IMemAlloc
 {
 public:
+#ifdef _WIN32
     virtual void* Alloc(size_t nSize) = 0;
     virtual void* Alloc(size_t nSize, const char* pFileName, int nLine) = 0;
 
@@ -17,4 +18,17 @@ public:
     virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
 
     virtual size_t GetSize(void* pMem) = 0;
+#else
+    virtual void* Alloc(size_t nSize) = 0;
+    virtual void* Realloc(void* pMem, size_t nSize) = 0;
+    virtual void Free(void* pMem) = 0;
+    virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize) = 0;
+
+    virtual void* Alloc(size_t nSize, const char* pFileName, int nLine) = 0;
+    virtual void* Realloc(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
+    virtual void Free(void* pMem, const char* pFileName, int nLine) = 0;
+    virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
+
+    virtual size_t GetSize(void* pMem) = 0;
+#endif
 };
