f9c11179d846699e469476b95d32a832fb8c37c1
SizzlingCalamari <jordan.cristi@gmail.com>
2025-02-18 22:45:40 -0500

    Removed IMemAlloc hack.
    MSVC likes to group virtual functions with the same name together. I hadn't declared enough of the class for the vtable layout to be correct, but it is now.

--- external/sourcesdk/public/tier0/memalloc.h
+++ external/sourcesdk/public/tier0/memalloc.h
@@ -4,21 +4,6 @@
 class IMemAlloc
 {
 public:
-#ifdef _WIN32
-    virtual void* Alloc(size_t nSize) = 0;
-    virtual void* Alloc(size_t nSize, const char* pFileName, int nLine) = 0;
-
-    virtual void* Realloc(void* pMem, size_t nSize) = 0;
-    virtual void* Realloc(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
-
-    virtual void Free(void* pMem) = 0;
-    virtual void Free(void* pMem, const char* pFileName, int nLine) = 0;
-
-    virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize) = 0;
-    virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
-
-    virtual size_t GetSize(void* pMem) = 0;
-#else
     virtual void* Alloc(size_t nSize) = 0;
     virtual void* Realloc(void* pMem, size_t nSize) = 0;
     virtual void Free(void* pMem) = 0;
@@ -30,5 +15,4 @@ public:
     virtual void* Expand_NoLongerSupported(void* pMem, size_t nSize, const char* pFileName, int nLine) = 0;
 
     virtual size_t GetSize(void* pMem) = 0;
-#endif
 };
